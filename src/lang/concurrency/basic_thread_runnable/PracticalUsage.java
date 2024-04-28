da7de0b0930bcbd8df7ec8a3a9fd5ddca9485aba
Sai Karthik <venkatasaikarthikeya@gmail.com>
2024-04-27 17:44:42 -0700

    multithreading the existing code and introduced need for daemon threads

--- src/lang/concurrency/basic_thread_runnable/PracticalUsage.java
+++ src/lang/concurrency/basic_thread_runnable/PracticalUsage.java
@@ -21,9 +21,27 @@ public class PracticalUsage {
                     before the computation of the previous number is complete.
 
                     To overcome this problem, we have the concept of Multi-threading
+
+                    Now, with the below updated approach, the computation of each nth prime number is done
+                    on a separate thread. Hence, the main thread is still active, and it accepts another
+                    input in parallel when the previous computation is still in progress. This way the main
+                    thread is not blocked by the long-running task.
+
+                    But, this approach introduces us to a new problem. When the long-running computation is
+                    being executed on another thread, if you try to exit the program using input 0, even though
+                    the main method returns, the program doesn't stop until the long-running computation is
+                    completed by the last thread. In other words, the main thread doesn't stop until all the
+                    threads it ever spawned come to a halt. This is a crucial problem because even though the user
+                    exits the application, the application will still run because the other threads it created
+                    are still running in the background.
+
+                    To address this problem, we have the concept of Daemon threads.
                 */
-                int result = PrimeNumberUtil.findNthPrime(n);
-                System.out.println(n + "th prime number is " + result);
+                Thread t = new Thread(() -> {
+                    int result = PrimeNumberUtil.findNthPrime(n);
+                    System.out.println(n + "th prime number is " + result);
+                });
+                t.start();
             }
         }
     }
