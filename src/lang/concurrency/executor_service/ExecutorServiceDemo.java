5b2371604acbee7f9703605c543210b92257480c
Sai Karthik <venkatasaikarthikeya@gmail.com>
2024-05-07 10:47:09 -0700

    executor service demo with fixed thread pool, to stop threads after main thread returns we used ThreadFactory to customize threads as daemon threads

--- src/lang/concurrency/executor_service/ExecutorServiceDemo.java
+++ src/lang/concurrency/executor_service/ExecutorServiceDemo.java
@@ -1,8 +1,6 @@
 package lang.concurrency.executor_service;
 
 import lang.concurrency.basic_thread_runnable.PrimeNumberUtil;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -13,13 +11,41 @@ public class ExecutorServiceDemo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        ExecutorService executorService = Executors.newFixedThreadPool(3);
+        // Implement Thread factory as a separate class
+        DaemonThreadFactory daemonThreadFactory = new DaemonThreadFactory();
+        // Pass the number of threads and thread factory instance to newFixedThreadPool()
+        ExecutorService executorService = Executors.newFixedThreadPool(3, daemonThreadFactory);
+
+        /*
+            // ThreadFactory is a functional interface. Hence, we can use Anonymous Inner Class
+            ExecutorService executorService1 = Executors.newFixedThreadPool(
+                3,
+                new ThreadFactory() {
+
+                    @Override
+                    public Thread newThread(Runnable r) {
+                        Thread t = new Thread(r);
+                        t.setDaemon(true);
+                        // other thread configurations
+                        return t;
+                    }
+                });
+        */
+
+        /*
+            // ThreadFactory is a functional interface. Hence, we can use Lambdas as well
+            ExecutorService executorService2 = Executors.newFixedThreadPool(3, (r) -> {
+                Thread t = new Thread(r);
+                t.setDaemon(true);
+                // other thread configurations
+                return t;
+            });
+        */
 
         while (true) {
             System.out.println("Enter the value of n to find nth prime number: ");
             int n = sc.nextInt();
             if (n == 0) {
-                executorService.shutdown();
                 break;
             } else {
                 Runnable r = () -> {
