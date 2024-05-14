3e6a46943dc4def7ac41fd1720b398e4e1e112d5
Sai Karthik <venkatasaikarthikeya@gmail.com>
2024-05-13 21:05:41 -0700

    collection interfaces

---
+++ src/lang/collections/pro/A_CollectionInterfaces.java
@@ -0,0 +1,22 @@
+package lang.collections.pro;
+
+public class A_CollectionInterfaces {
+
+    public static void main(String[] args) {
+
+        /*
+
+                         ------------Collection Interfaces-----------
+
+                                          Collection                                              Map
+                                               |                                                   |
+                -----------------------------------------------------------------------        Sorted Map
+               |                    |                           |                       |
+              Set                  List                        Queue                  Deque
+               |
+            Sorted Set
+        */
+
+
+    }
+}
